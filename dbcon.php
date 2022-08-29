7fbe7c7c8519cc98e7c2662f95fe4fc79d9756b9
rainzseven <rainzpedia@gmail.com>
2022-08-29 20:51:29 +0700

    simple toko online whatsapp

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$tableprefix = "whatsstore_"; //Anda dapat mengubah awalan tabel ini, jangan gunakan spasi putih, gunakan garis bawah sebagai gantinya misalnya whatsstore22_is_my_table_prefix_
+$databasename = ""; //nama database
+$dbuser = ""; //nama user database
+$dbpassword = ""; //pw user database
